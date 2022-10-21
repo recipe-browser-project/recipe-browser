2d46709c70d44d858ad10a092dc2c6759effe2e6
coderjohn69 <dheerajgururani18@gmail.com>
2022-10-21 17:15:00 +0200

    Login Page Completed , logo will be added later

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/Login.vue'
+</script>
+
+<template>
+
+  <HelloWorld msg="Recipe Browser Login" />
+</template>
+
+<style scoped>
+
+</style>
